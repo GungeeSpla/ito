110ee8d50b899650ac721737009c4bb356767539
ガンジー <gandhi.salmonrun@gmail.com>
2025-03-25 09:29:19 +0900

    Tailwind CSSを導入

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,12 @@ function App() {
   return (
     <div>
       <h1>ito Online</h1>
+      <div className="text-center mt-10">
+        <h1 className="text-3xl font-bold text-purple-600">Hello Tailwind!</h1>
+        <button className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+          おにいちゃん押して～♡
+        </button>
+      </div>
       <input
         type="text"
         placeholder="ニックネームを入力"
