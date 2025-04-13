0c93233db00d33433e6764932daa189c076c294e
ガンジー <gandhi.salmonrun@gmail.com>
2025-04-14 04:13:46 +0900

    fix: サイト内の日本語の微調整

--- src/App.tsx
+++ src/App.tsx
@@ -237,7 +237,7 @@ function App() {
       <div className="relative w-full text-center px-4 overflow-hidden">
         {/*-------- 見出し --------*/}
         <h2 className="text-3xl font-bold text-shadow-md mt-0 mb-4">
-          数字の大きさを感覚でたとえて言葉にするゲーム
+          数字の大きさを言葉でたとえて価値観を共有するゲーム
         </h2>
         <p className="text-center text-white text-shadow-md my-6">
           <span>ニックネームを入力して、ルームを作成してください。</span>
