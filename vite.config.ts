dede5ac69eb503444f56493fafc176439c5fb4e3
ガンジー <gandhi.salmonrun@gmail.com>
2025-04-19 03:52:12 +0900

    feat: PWA対応

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,39 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import pkg from "./package.json";
+import { VitePWA } from "vite-plugin-pwa";
 const buildDate = new Date().toISOString();
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "数字の大きさを言葉でたとえて価値観を比べるゲーム",
+        short_name: "価値観比較ゲーム",
+        description:
+          "itoレインボーと同じルールのゲームがオンラインで遊べる非公式サイトです。数字の大きさを言葉でたとえて価値観を比べよう！あなたの「大きい」は、わたしの「小さい」かも？",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#121212",
+        orientation: "any",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
     __BUILD_DATE__: JSON.stringify(buildDate),
